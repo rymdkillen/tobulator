276be0b9761b516fe1484336435074bdea0492e6
Tobias <hanssongt@gmail.com>
2016-07-24 20:37:27 +0200

    More stuff

--- src/Main.java
+++ src/Main.java
@@ -11,79 +11,22 @@ public class Main {
 
         String input = scanner.nextLine();
 
-
-        //gör bara en fucking string som du vandrar igenom och sorterar ur på nåt jävla sätt... nummer i en array eller nåt, operator i en annan
-
-
-
-        String[] tokens = input.split("\\+");
-
-        double token1 = Double.parseDouble(tokens[0]);
-        double token2 = Double.parseDouble(tokens[1]);
-
-        double answer = calculator.add(token1, token2);
-
-        System.out.println(answer);
-
-
-        /*double inputA = scanner.nextDouble();
-        //char c = scanner.next().charAt(0);
-        //scanner.nextLine();
-        double inputB = scanner.nextDouble();
-        scanner.nextLine();
-
-        double answer = calculator.add(inputA, inputB);
-
-        System.out.println(answer);*/
-
-        /*System.out.println(inputA);
-        System.out.println(c);
-        System.out.println(inputB);*/
+        System.out.println(isNumber(input));
+    }
 
 
 
+    public static ArrayList isNumber(String jocki) {
+        //Scanner igen = new Scanner(jocki);
 
+        ArrayList<String> bosse = new ArrayList<String>();
 
-
-        /*String input = scanner.nextLine();
-        String[] parts = input.split("");
-
-        for (int i = 0; i < parts.length; i++) {
-            System.out.println(parts[i]);
+        if (jocki.startsWith("1")) {
+            bosse.add("1");
         }
-
-        ArrayList<Integer> numlist = new ArrayList<>();
-
-        public void isNumber() {
-            if (input.startsWith("1")) {
-                numlist.add(1);
-            }
+        else if (jocki.startsWith("2")) {
+            bosse.add("2");
         }
-
-        System.out.println(numlist);*/
-
-
-
-
-
-
-
-        /*int[] n1 = new int[parts.length];
-        for (int n = 0; n < parts.length; n++) {
-            n1[n] = Integer.parseInt(parts[n]);
-        }
-
-
-        //kolla sen om det är en siffra först, sen om det är ett + osv
-        //om + gör plus, om annat gör det
-
-
-        int sum = 0;
-
-        for (int num : n1) {
-            sum = sum + num;
-        }
-
-        System.out.println(sum);*/
+        return bosse;
     }
 }
