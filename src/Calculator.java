067c00995f73798716dcbbbf9c119fb2976310e0
hansson <hansson@spotify.com>
2016-07-25 17:21:15 +0200

    Even more

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,72 @@
-public class Calculator {
+import java.util.Scanner;
+import java.util.Stack;
 
-    double add(double a, double b) {
-        double answer = a + b;
-        return answer;
+public class Calculator {
+    private String token = "";
+
+    public double readInput(String exp) {
+        Scanner input = new Scanner(exp);
+
+        Stack<Double> number = new Stack<Double>();
+        Stack<String> operator = new Stack<String>();
+
+        while (input.hasNext()) {
+            token = input.next();
+
+            if (isNumber(token)) {
+                number.push(Double.parseDouble(token));
+                System.out.println(number);
+            }
+            else if (token.equals("+")) {
+                operator.push(token);
+                System.out.println(operator);
+            }
+        }
+        return number.pop();
     }
 
+    public boolean isOperator(String exp) {
+        if (exp.equals("+")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isNumber(String exp) {
+        if (exp.startsWith("0")) {
+            return true;
+        }
+        else if (exp.startsWith("1")) {
+            return true;
+        }
+        else if (exp.startsWith("2")) {
+            return true;
+        }
+        else if (exp.startsWith("3")) {
+            return true;
+        }
+        else if (exp.startsWith("4")) {
+            return true;
+        }
+        else if (exp.startsWith("5")) {
+            return true;
+        }
+        else if (exp.startsWith("6")) {
+            return true;
+        }
+        else if (exp.startsWith("7")) {
+            return true;
+        }
+        else if (exp.startsWith("8")) {
+            return true;
+        }
+        else if (exp.startsWith("9")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,12 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
-
-        System.out.println("plussa ihop nåra tal:");
+        Calculator calculate = new Calculator();
 
         String input = scanner.nextLine();
-
-        System.out.println(isNumber(input));
-    }
-
-
-
-    public static ArrayList isNumber(String jocki) {
-        //Scanner igen = new Scanner(jocki);
-
-        ArrayList<String> bosse = new ArrayList<String>();
-
-        if (jocki.startsWith("1")) {
-            bosse.add("1");
-        }
-        else if (jocki.startsWith("2")) {
-            bosse.add("2");
-        }
-        return bosse;
+        calculate.readInput(input);
     }
 }
